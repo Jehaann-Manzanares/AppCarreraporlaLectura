5a90f37a77d6d0d11d549cbde9e95c029d87957a
info@codecreativenicaragua.com <info@codecreativenicaragua.com>
2018-01-23 16:42:50 -0600

    Agregando Submenu nivel 1

--- app/src/main/java/com/te/unan/appestudianteswm/MenuNivel1.java
+++ app/src/main/java/com/te/unan/appestudianteswm/MenuNivel1.java
@@ -1,18 +1,32 @@
 package com.te.unan.appestudianteswm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 public class MenuNivel1 extends AppCompatActivity {
 
+    ImageView act1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_menu_nivel1);
+
+        act1 = (ImageView) findViewById(R.id.actividad1);
+
+        act1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
